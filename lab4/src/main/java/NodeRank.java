ecfe7710c039f76a6284cc9d22cb3a832f1eebe2
dbrcina <dbrcina@donotreply.com>
2021-04-23 21:39:44 +0100

    Small update

--- lab4/src/main/java/NodeRank.java
+++ lab4/src/main/java/NodeRank.java
@@ -1,7 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
 
 public class NodeRank {
@@ -19,70 +21,96 @@ public class NodeRank {
 
     private static final String SPLIT_DEL = "\\s+";
     private static final int N_ITERATIONS = 100;
-    private static final StringBuilder RESULTS = new StringBuilder();
+    private static final StringBuilder SB = new StringBuilder();
     private static final String LINE_SEP = System.lineSeparator();
 
-    private static int n;
-    private static double beta;
-    private static int[][] adjacencyList;
-    private static double[][] ranksPerIterations;
-    private static int[][] queries;
+    private static class DataModel {
+        private final int n;
+        private final double beta;
+        private final List<int[]> adjacencyList;
+        private final List<List<Query>> queriesPerIterations;
+        private final double[] results;
 
-    public static void main(String[] args) throws IOException {
-        parseInput();
-        pageRankAlgorithm();
-        processQueries();
-        System.out.println(RESULTS);
+        private DataModel(int n, double beta, List<int[]> adjacencyList, List<List<Query>> queriesPerIterations) {
+            this.n = n;
+            this.beta = beta;
+            this.adjacencyList = adjacencyList;
+            this.queriesPerIterations = queriesPerIterations;
+            this.results = new double[n];
+        }
     }
 
-    private static void parseInput() throws IOException {
+    private static class Query {
+        private final int id;
+        private final int ni;
+        private final int ti;
+
+        private Query(int id, int ni, int ti) {
+            this.id = id;
+            this.ni = ni;
+            this.ti = ti;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        DataModel dataModel = parseInput();
+        processQueries(dataModel);
+        Arrays.stream(dataModel.results).forEach(result -> SB.append(String.format(Locale.US, "%.10f%n", result)));
+        SB.setLength(SB.length() - LINE_SEP.length());
+        System.out.println(SB);
+    }
+
+    private static DataModel parseInput() throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             // Parse n and beta.
             String[] parts = readLineAndSplit(br);
-            n = Integer.parseInt(parts[0]);
-            beta = Double.parseDouble(parts[1]);
-            adjacencyList = new int[n][];
+            int n = Integer.parseInt(parts[0]);
+            double beta = Double.parseDouble(parts[1]);
+            List<int[]> adjacencyList = new ArrayList<>(n);
             // Parse connections.
             for (int i = 0; i < n; i++) {
-                adjacencyList[i] = parseLine(br);
+                adjacencyList.add(parseLine(br));
             }
-            ranksPerIterations = new double[N_ITERATIONS + 1][n];
             // Parse q.
             int q = parseLine(br)[0];
-            queries = new int[q][];
+            List<List<Query>> queriesPerIterations = new ArrayList<>(N_ITERATIONS);
+            for (int i = 0; i < N_ITERATIONS; i++) {
+                queriesPerIterations.add(new ArrayList<>());
+            }
             // Parse queries.
             for (int i = 0; i < q; i++) {
-                queries[i] = parseLine(br);
+                int[] parsedData = parseLine(br);
+                Query query = new Query(i, parsedData[0], parsedData[1] - 1);
+                queriesPerIterations.get(query.ti).add(query);
             }
+            return new DataModel(n, beta, adjacencyList, queriesPerIterations);
         }
     }
 
-    private static void pageRankAlgorithm() {
-        double[] initialRanks = new double[n];
-        Arrays.fill(initialRanks, 1.0 / n);
-        ranksPerIterations[0] = initialRanks;
-        for (int i = 1; i <= N_ITERATIONS; i++) {
-            double[] previousRanks = ranksPerIterations[i - 1];
+    private static void processQueries(DataModel dataModel) {
+        int n = dataModel.n;
+        double beta = dataModel.beta;
+        double teleport = (1 - beta) / n;
+        List<int[]> adjacencyList = dataModel.adjacencyList;
+        List<List<Query>> queriesPerIterations = dataModel.queriesPerIterations;
+        double[] results = dataModel.results;
+        double[] previousRanks = new double[n];
+        Arrays.fill(previousRanks, 1.0 / n);
+        for (List<Query> queries : queriesPerIterations) {
             double[] newRanks = new double[n];
-            Arrays.fill(newRanks, (1 - beta) / n);
-            for (int j = 0; j < n; j++) {
-                int[] jthConnections = adjacencyList[j];
-                double jthRank = beta * previousRanks[j] / jthConnections.length;
-                for (int connection : jthConnections) {
-                    newRanks[connection] += jthRank;
+            Arrays.fill(newRanks, teleport);
+            for (int i = 0; i < n; i++) {
+                int[] connections = adjacencyList.get(i);
+                double rank = beta * previousRanks[i] / connections.length;
+                for (int connection : connections) {
+                    newRanks[connection] += rank;
                 }
             }
-            ranksPerIterations[i] = newRanks;
+            previousRanks = newRanks;
+            for (Query query : queries) {
+                results[query.id] = newRanks[query.ni];
+            }
         }
-    }
-
-    private static void processQueries() {
-        for (int[] query : queries) {
-            int ni = query[0];
-            int ti = query[1];
-            RESULTS.append(String.format(Locale.US, "%.10f%n", ranksPerIterations[ti][ni]));
-        }
-        RESULTS.setLength(RESULTS.length() - LINE_SEP.length());
     }
 
     private static String[] readLineAndSplit(BufferedReader br) throws IOException {
